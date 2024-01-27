1b398fa3a94a63eefe160a348d58aa8994a0eff0
Jiahao Liu <jiahaoliuliu@gmail.com>
2024-01-27 18:34:03 +0100

    JL Added a simple test and bubble sort algorithm

--- src/main/kotlin/sort/BubbleSort.kt
+++ src/main/kotlin/sort/BubbleSort.kt
@@ -1,4 +1,34 @@
 package sort
 
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Bubble sort gets one element per time, going through the list and swap its position with others when
+ * needed.
+ *
+ */
 class BubbleSort {
+    private fun bubbleSort(mutableList: MutableList<Int>) {
+        for (i in 0 until mutableList.size - 1) {
+            for (j in mutableList.size - 1 downTo i + 1) {
+                if (mutableList[j] < mutableList[j - 1]) {
+                    swap(mutableList, j, j-1)
+                }
+            }
+            println()
+        }
+    }
+
+    private fun swap(list: MutableList<Int>, i: Int, j: Int) {
+        val tmp = list[i]
+        list[i] = list[j]
+        list[j] = tmp
+    }
+
+
+    @Test
+    fun test() {
+        runAllTests(::bubbleSort)
+    }
 }
